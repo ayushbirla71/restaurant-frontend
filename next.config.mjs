f0951d7c1ffc63212c8cd23d9b630e22fd16f37c
ayush birla <ayushbirla71@gmail.com>
2025-12-19 00:05:44 +0530

    workng code

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",        // 👈 REQUIRED for static deploy
+
   typescript: {
     ignoreBuildErrors: true,
   },
-  images: {
-    unoptimized: true,
-  },
-}
 
-export default nextConfig
+  images: {
+    unoptimized: true,     // 👈 REQUIRED for static hosting
+  },
+};
+
+export default nextConfig;
